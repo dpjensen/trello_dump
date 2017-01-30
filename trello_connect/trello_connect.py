f355504a246e136cd3d6d97cb7b3a5ff5fccc9ab
Derek Jensen <derek@dpjensen.net>
2017-01-29 18:44:47 -0600

    cut down on API called, cleared up dead code

--- trello_connect/trello_connect.py
+++ trello_connect/trello_connect.py
@@ -7,17 +7,20 @@ class TrelloConnect():
     A very simple class for connecting to the trello API.
     """
 
-    def __init__(self, token, key):
+    def __init__(self, token, key, verb):
         self.token = token
         self.key = key
         self.http = urllib3.PoolManager(
-                    cert_reqs='CERT_REQUIRED',
-                    ca_certs=certifi.where()
+            cert_reqs='CERT_REQUIRED',
+            ca_certs=certifi.where()
         )
+        self.verbose = verb
         #get member info.
-        resp =  self.http.request("GET",
-                                "https://api.trello.com/1/members/me?fields=username&boards=all&board_fields=name&key={}&token={}"
-                                .format(self.key, self.token))
+        request = "https://api.trello.com/1/members/me?fields=username&boards=all&board_fields=name&key={}&token={}".format(self.key, self.token)
+        if self.verbose:
+            print(request)
+        resp = self.http.request("GET", request)
+
         if resp.status != 200:
             raise RuntimeError("Trello returned {}: {}".format(resp.status, resp.data))
         data_dict = json.loads(resp.data.decode("UTF-8"))
@@ -32,9 +35,10 @@ class TrelloConnect():
         Returns a dataset on a card, given an ID.
         This returns a large dataset, core application requres at least all history
         """
-        resp = self.http.request("GET",
-                                "https://api.trello.com/1/cards/{}?actions=all&actions_display=true&action_memberCreator_fields=username&members=true&member_fields=username,status&attachments=true&fields=all&checklists=all&list=true&actions_limit=50&key={}&token={}"
-                                .format(cid, self.key, self.token))
+        request = "https://api.trello.com/1/cards/{}?actions=all&actions_display=true&action_memberCreator_fields=username&members=true&member_fields=username,status&attachments=true&fields=all&checklists=all&list=true&actions_limit=50&key={}&token={}".format(cid, self.key, self.token)
+        if self.verbose:
+            print(request)
+        resp = self.http.request("GET", request)
         data_dict = json.loads(resp.data.decode("UTF-8"))
         return data_dict
 
@@ -44,10 +48,10 @@ class TrelloConnect():
         """
         if board not in self.boards:
             print("The board '{}' could not be found.".format(board))
-
-        resp = self.http.request("GET",
-                                "https://api.trello.com/1/boards/{}?lists=open&list_fields=all&fields=name,desc&key={}&token={}"
-                                .format(self.boards[board], self.key, self.token))
+        request = "https://api.trello.com/1/boards/{}?lists=open&list_fields=all&fields=name,desc&key={}&token={}".format(self.boards[board], self.key, self.token)
+        if self.verbose:
+            print(request)
+        resp = self.http.request("GET", request)
         data_dict = json.loads(resp.data.decode("UTF-8"))
         return data_dict["lists"]
 
@@ -55,8 +59,9 @@ class TrelloConnect():
         """
         Get the cards in a list
         """
-        resp = self.http.request("GET",
-                                "https://api.trello.com/1/lists/{}?fields=name&cards=open&card_fields=all&key={}&token={}"
-                                .format(list_id, self.key, self.token))
+        request = "https://api.trello.com/1/lists/{}?fields=name&cards=open&card_fields=all&key={}&token={}".format(list_id, self.key, self.token)
+        if self.verbose:
+            print(request)
+        resp = self.http.request("GET", request)
         data_dict = json.loads(resp.data.decode("UTF-8"))
         return data_dict
